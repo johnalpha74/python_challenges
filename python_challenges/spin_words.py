bffc347f3884042dbdfb95acd65c3a85c797ea1c
John Alpha <56693473+johnalpha74@users.noreply.github.com>
2022-10-25 19:13:33 +0200

    Update spin_words.py

--- python_challenges/spin_words.py
+++ python_challenges/spin_words.py
@@ -1,7 +1,5 @@
 '''
-- Write a function that takes in a string of one or more words, 
-and returns the same string, 
-- but with all five or more letter words reversed 
+- Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed 
 - (Just like the name of this Kata- Stop gninnipS My sdroW!). 
 - Strings passed in will consist of only letters and spaces. 
 - Spaces will be included only when more than one word is present.
